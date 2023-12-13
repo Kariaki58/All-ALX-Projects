30fb7a37319241241c2212e812784a049f5f521f
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-13 05:24:33 +0100

    using the function

--- alx-low_level_programming/0x06-pointers_arrays_strings/2-strncpy.c
+++ alx-low_level_programming/0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 
 /**
@@ -10,11 +11,5 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
-
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+    return (strncpy(dest, src, n));
 }
