cf24ccf26a60a4bc723592499509d031227c8bbd
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-13 05:27:25 +0100

    strcmp

---
+++ alx-low_level_programming/0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strcmp - strcmp function
+ * @s1: s1
+ * @s2: s2
+ * Return: strcmp
+*/
+int _strcmp(char *s1, char *s2)
+{
+    return (strcmp(s1, s2));
+}
